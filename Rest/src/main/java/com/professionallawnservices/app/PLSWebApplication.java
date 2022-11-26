e96c7281f1abccf88407b8a12e323b34eb47ba76
Jordan Hinson <jhinson2012@gmail.com>
2022-11-26 17:59:46 -0500

    Completed the example login using spring security

---
+++ Rest/src/main/java/com/professionallawnservices/app/PLSWebApplication.java
@@ -0,0 +1,13 @@
+package com.professionallawnservices.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PLSWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PLSWebApplication.class, args);
+	}
+
+}
---
+++ Rest/src/test/java/com/professionallawnservices/app/PLSWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professionallawnservices.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PLSWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
