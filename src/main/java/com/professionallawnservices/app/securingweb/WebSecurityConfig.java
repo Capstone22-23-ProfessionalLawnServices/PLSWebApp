59c522998b3be0c35dde987c246df870e1f91cf1
Jordan Hinson <jhinson2012@gmail.com>
2022-12-20 09:38:37 -0500

    Added multiple user logins and page redirection after form login

--- src/main/java/com/professionallawnservices/app/securingweb/WebSecurityConfig.java
+++ src/main/java/com/professionallawnservices/app/securingweb/WebSecurityConfig.java
@@ -7,8 +7,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,7 @@ public class WebSecurityConfig {
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
+                        .defaultSuccessUrl("/greeting", true)
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
@@ -39,14 +45,20 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
-        return new InMemoryUserDetailsManager(user);
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.withUsername("employee").password(passwordEncoder().encode("password"))
+                .roles("EMPLOYEE", "USER").build());
+        userDetailsList.add(User.withUsername("manager").password(passwordEncoder().encode("password"))
+                .roles("MANAGER", "USER").build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
