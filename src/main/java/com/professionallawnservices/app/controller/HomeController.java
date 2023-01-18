c7a553c48ed1f5083baf0bb66815298a0bd2c9a8
Jordan Hinson <jhinson2012@gmail.com>
2023-01-17 19:05:42 -0500

    Removed unused import in the home controller

--- src/main/java/com/professionallawnservices/app/controller/HomeController.java
+++ src/main/java/com/professionallawnservices/app/controller/HomeController.java
@@ -2,17 +2,12 @@ package com.professionallawnservices.app.controller;
 
 import com.professionallawnservices.app.helpers.SecurityHelpers;
 import com.professionallawnservices.app.enums.RolesEnum;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Collection;
 
 import static com.professionallawnservices.app.enums.RolesEnum.*;
+
 
 @Controller
 public class HomeController {
