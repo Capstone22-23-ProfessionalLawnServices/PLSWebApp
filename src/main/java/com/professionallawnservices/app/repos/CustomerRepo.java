6f2641f3c550b922c6aa57dd76afeed5df4c79f2
Jordan Hinson <jhinson2012@gmail.com>
2023-01-19 16:14:19 -0500

    Refactored controller directory

--- src/main/java/com/professionallawnservices/app/repos/CustomerRepo.java
+++ src/main/java/com/professionallawnservices/app/repos/CustomerRepo.java
@@ -1,2 +1,7 @@
-package com.professionallawnservices.app.repos;public interface CustomerRepo {
+package com.professionallawnservices.app.repos;
+
+import com.professionallawnservices.app.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 }
