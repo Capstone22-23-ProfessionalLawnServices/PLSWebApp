6c1a39499c72c30ef31b6d640a814cdfa44a7361
Jordan Hinson <jordan.hinson@g.fmarion.edu>
2023-01-28 10:21:05 -0500

    Fixed some ui issues and added the add contact and contacts pages

--- src/main/java/com/professionallawnservices/app/controllers/AccountController.java
+++ src/main/java/com/professionallawnservices/app/controllers/AccountController.java
@@ -1,16 +1,24 @@
 package com.professionallawnservices.app.controllers;
 
+import com.professionallawnservices.app.enums.RolesEnum;
+import com.professionallawnservices.app.helpers.SecurityHelpers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static com.professionallawnservices.app.enums.RolesEnum.MANAGER;
+
 @Controller
 public class AccountController {
 
     @GetMapping("/account")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String accountView(Model model) {
+        RolesEnum user = SecurityHelpers.getPrimaryUserRole();
+
+        model.addAttribute("userAccessLevel", user.accessLevel);
+        model.addAttribute("managerAccessLevel", MANAGER.accessLevel);
+
         return "account";
     }
 
--- src/main/java/com/professionallawnservices/app/controllers/CustomerController.java
+++ src/main/java/com/professionallawnservices/app/controllers/CustomerController.java
@@ -1,6 +1,7 @@
 package com.professionallawnservices.app.controllers;
 
 import com.professionallawnservices.app.helpers.SecurityHelpers;
+import com.professionallawnservices.app.models.Contact;
 import com.professionallawnservices.app.models.Customer;
 import com.professionallawnservices.app.repos.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.professionallawnservices.app.enums.RolesEnum;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -23,10 +27,22 @@ public class CustomerController {
 
     private static final String managerRole = MANAGER.roleName;
 
-    @GetMapping(value = "/getCustomers")
-    //@PreAuthorize("hasRole('MANAGER')")
-    public List<Customer> getUsers(Model model) {
-        return customerRepo.findAll();
+    @GetMapping("/customers")
+    public String customersView(Model model) {
+        model.addAttribute("customers", customerRepo.findAll());
+        return "customers";
+    }
+
+    @GetMapping("/add-customer")
+    public String addCustomerView(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "add-customer";
+    }
+
+    @PostMapping("/create-customer")
+    public RedirectView createContact(@ModelAttribute Customer customer) {
+        customerRepo.save(customer);
+        return new RedirectView("/add-customer");
     }
 
     /*
