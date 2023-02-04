7b5aa48f576200a58ca53a546f5c0f62e4c673d5
Jordan Hinson <jhinson2012@gmail.com>
2023-02-04 16:39:06 -0500

    Added api key import from application.settings file

--- src/main/java/com/professionallawnservices/app/services/HomeService.java
+++ src/main/java/com/professionallawnservices/app/services/HomeService.java
@@ -2,6 +2,7 @@ package com.professionallawnservices.app.services;
 
 import com.professionallawnservices.app.models.openweather.Interval;
 import com.professionallawnservices.app.models.openweather.OpenWeatherResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,10 +11,13 @@ import java.util.Date;
 @Service
 public class HomeService {
 
+    @Value("${apikey.openweatherapi}")
+    private String apikey;
+
     public void getCalendarWeather() {
 
         final String uri = "http://api.openweathermap.org/data/2.5/forecast?lat=34.19&lon=-79.76&" +
-                "appid=cb80dc8fdd6d29382fe2a9c100b6f7f8&units=imperial";
+                "appid=" + apikey + "&units=imperial";
 
         RestTemplate restTemplate = new RestTemplate();
         String reqBody = "";
