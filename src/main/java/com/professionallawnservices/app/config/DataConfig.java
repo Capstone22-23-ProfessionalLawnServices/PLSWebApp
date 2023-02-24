b957ded329f9ff1f11213648e892bc937561bd91
Jordan Hinson <jordan.hinson@g.fmarion.edu>
2023-02-24 15:17:22 -0500

    Added a datasource builder

--- src/main/java/com/professionallawnservices/app/config/DataConfig.java
+++ src/main/java/com/professionallawnservices/app/config/DataConfig.java
@@ -1,18 +1,51 @@
 package com.professionallawnservices.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class DataConfig {
 
+    /*
     @Bean
     @ConfigurationProperties("app.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
+
+     */
+
+    @Value("${app.datasource.jdbc-url}")
+    private String url;
+
+    @Value("${app.datasource.username}")
+    private String username;
+
+    @Value("${app.datasource.password}")
+    private String password;
+
+    @Primary
+    @Bean
+    public DataSource mysqlDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.url("jdbc:mysql://localhost/testdb");
+//        dataSourceBuilder.username("dbuser");
+//        dataSourceBuilder.password("dbpass");
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+
+    char charcter = (char) 4;
 }
