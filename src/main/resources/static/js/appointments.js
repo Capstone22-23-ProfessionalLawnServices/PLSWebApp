28ba89ec6d94c3aed043f39be406e2f7c001a21a
Jordan Hinson <jhinson2012@gmail.com>
2023-03-19 10:11:42 -0400

    Impletemented paging for the appointment page

--- src/main/resources/static/js/appointments.js
+++ src/main/resources/static/js/appointments.js
@@ -1,5 +1,27 @@
 function rowClick(e) {
     let rowId = e.target.parentNode.getAttribute("value");
 
-    window.location.href = ("appointments/update/" + rowId);
+    window.location.href = ("/appointments/update/" + rowId);
+}
+
+function nextPage(e) {
+    let pageNumber = Number(document.getElementById("current-page").value);
+    let nextPage = pageNumber;
+
+    window.location.href = ("/appointments?page=" + nextPage);
+}
+
+function previousPage(e) {
+    let pageNumber = Number(document.getElementById("current-page").value);
+    let previousPage = pageNumber - 2;
+
+    window.location.href = ("/appointments?page=" + previousPage);
+}
+
+function pageInBox(e) {
+    if (e.keyCode === 13) {
+        let pageNumber = Number(document.getElementById("current-page").value);
+
+        window.location.href = ("/appointments?page=" + pageNumber);
+    }
 }
