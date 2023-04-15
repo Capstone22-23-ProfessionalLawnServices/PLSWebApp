d11a83c4e2fff31a4dda3ead15d86c6c766c0ecb
Jordan Hinson <jhinson2012@gmail.com>
2023-04-15 14:29:45 -0400

    Changed instances of statistics to reports

--- src/main/java/com/professionallawnservices/app/controllers/ReportsController.java
+++ src/main/java/com/professionallawnservices/app/controllers/ReportsController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/statistics")
-public class StatisticsController {
+@RequestMapping("/reports")
+public class ReportsController {
 
     @GetMapping("")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "statistics";
+        return "reports";
     }
 
 }
