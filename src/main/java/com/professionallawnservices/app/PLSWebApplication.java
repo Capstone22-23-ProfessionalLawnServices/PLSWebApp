8e0d094f2685e68c51ad555e2661c77386c6ee5f
Jordan Hinson <jhinson2012@gmail.com>
2023-07-15 19:48:47 -0400

    Updated application.properties and removed tag from startup

--- src/main/java/com/professionallawnservices/app/PLSWebApplication.java
+++ src/main/java/com/professionallawnservices/app/PLSWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-dev.properties")
 public class PLSWebApplication {
 
 	public static void main(String[] args) {
